03e5dce069848700d00db5d6017a1d61732d3773
Chris Modzelewski <cmodzelewski@marketsight.internal>
2018-11-12 23:24:27 -0500

    Updated setup.py with jsonschema dependency.

--- setup.py
+++ setup.py
@@ -146,7 +146,8 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'regex;python_version<"3"',
+        'jsonschema',
+        'regex;python_version<"3"'
     ],
 
     # List additional groups of dependencies here (e.g. development
